a89d6e7b0b89215457525763ddfa1c7d330de198
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-31 19:18:48 -0800

    Update lecture2.py

--- Lecture2/lecture2.py
+++ Lecture2/lecture2.py
@@ -1,8 +1,15 @@
 #Over view
 """Explain what a value is and how to write statements that assign values"""
 # name and value; type is decided by the value;
-# '=' means 'gets the value'; '==' means 'is equivalent to'; 'is' means 'the same memory loacation'
+# '=' means 'gets the value'; '==' means 'is equivalent to'; 
+# 'is' means 'the same memory loacation', point to the same object;
 """Distinguish between basic types, including integers, floats, and strings"""
+# ints can be arbitrarily large
+# floats are stored in ISO/IEC format from C language
 """Manipulate vars using arithmetic operators to perform calculations"""
+#precision
+#string concatenation
+#use \ for escape character
 """Describe and use std conventions for naming variables and writing code"""
+
 """Define a Boolean expresstion and understand its use"""
