6235fc839b06d4df5f53c00911eb6ea6db642f01
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-01 12:18:45 -0800

    Update lecture2.py

--- Lecture2/lecture2.py
+++ Lecture2/lecture2.py
@@ -15,3 +15,24 @@
 """Define a Boolean expresstion and understand its use"""
 # question:  is, python's location
 # compare: use unicode for character comparing
+def main():
+    pizzas = int(input("how many pizzas"))
+    people = int(input("how many people"))
+    try:
+        slices_average = pizzas * 8 / people
+    except ZeroDivisionError:
+        print("people cannot be 0")
+
+"""Common Errors"""
+# Syntax Error
+# Logic Error: Tautology; Contradiction; 
+# Confusion, inefficient booleans( use several logic laws to improve)
+# Inefficient algorithms
+# Short circuit evaluation
+
+
+"""Patterns in Programming"""
+# Decompose problem into simpler subproblems
+# split the hypothesis space (eg binary search)
+# Modify existing solution to analogous or related problem 
+# Debug an almost-right approach: isolate each; common bug names; temporary print statement; debugging tools(tracing(in recursion), single-stepping)
