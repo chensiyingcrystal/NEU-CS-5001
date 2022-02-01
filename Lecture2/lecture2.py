1d2c5e4147140ff1f889cea12b77b819cca943f8
Siying Chen <chensiyingcrystal@gmail.com>
2022-01-31 19:01:08 -0800

    Update lecture2.py

---
+++ Lecture2/lecture2.py
@@ -0,0 +1,8 @@
+#Over view
+"""Explain what a value is and how to write statements that assign values"""
+# name and value; type is decided by the value;
+# '=' means 'gets the value'; '==' means 'is equivalent to'; 'is' means 'the same memory loacation'
+"""Distinguish between basic types, including integers, floats, and strings"""
+"""Manipulate vars using arithmetic operators to perform calculations"""
+"""Describe and use std conventions for naming variables and writing code"""
+"""Define a Boolean expresstion and understand its use"""
