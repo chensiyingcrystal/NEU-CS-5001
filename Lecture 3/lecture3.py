d0a21a3872f8c87d97ed2b45e8cef967c6f2a48f
Siying Chen <chensiyingcrystal@gmail.com>
2022-02-07 18:13:31 -0800

    Update lecture3.py

---
+++ Lecture 3/lecture3.py
@@ -0,0 +1,8 @@
+"""funtions and testing"""
+# explain what a function is 
+# define the key parts of a funtion
+# call a function 
+# design and write sample functions
+# implement tests to debug functions by comparing expected vs. actual values
+
+# two textbooks and exercises -- midterm exam
