1ccaddd2b88b6b6a3101741b8fa1fa751ae737ec
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-07 19:55:30 -0800

    update

---
+++ Lecture6/Lecture6.py
@@ -0,0 +1,13 @@
+"""For loop"""
+# 1. range function
+# range object: range(first, stop, step)
+# stop not inclusive; step can be -1
+# 2. Iterating through a list
+#list is mutable, so it can be changed in a for loop
+# not all loops can be wrote as a for loop
+#for example, something that you should validate user's input
+#if user define l as a tuple, then it cannot be changed in a for loop
+# 3. iteration wrap-up
+# iteration works well on "linear" or "flat" data collections
+# Recursion are more convenient for tree-like data structures
+
