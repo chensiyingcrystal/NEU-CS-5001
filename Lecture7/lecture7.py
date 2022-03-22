80dd9da6244f1aa051898e48e1323eb80e07f1ca
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-21 19:45:37 -0700

    Update lecture7.py

--- Lecture7/lecture7.py
+++ Lecture7/lecture7.py
@@ -3,13 +3,13 @@ base case
 leap of faith"""
 
 """write an iterative and a recursive function for calculating factorial of n"""
-def n_rec_fact(n):
+def rec_fact(n):
     if (n < 2):
         return 1
     else:
-        return n * n_rec_fact(n - 1)
+        return n * rec_fact(n - 1)
 
-def n_ite_fact(n):
+def ite_fact(n):
     i = 1
     product = 1
     while (i <= n):
@@ -17,8 +17,22 @@ def n_ite_fact(n):
         i += 1
     return product
 
-print(n_rec_fact(3))
-print(n_ite_fact(3))
+print(rec_fact(3))
+print(ite_fact(3))
 
+"""properties: somtimes easiest way to think about a problem
+sometimes can be highly inefficient, especially in python"""
 
+"""the structural parts of a recursive solution:
+base case
+recursive case
+"""
+
+"""translate design from iterative to recursive and vice versa, where applicable"""
+
+"""Recursion helps with trees:
+maximize from branches to root
+iterating presents the challenge of not knowing th depth of the tree in advance
+recursion works elegantly, regardless of the depth of the tree
+"""
 
