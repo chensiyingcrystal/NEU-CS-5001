90bc313810c6390360e1874a28aa42ce529d6bca
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-28 18:54:05 -0700

    update

---
+++ Lecture8/Lecture8.py
@@ -0,0 +1,14 @@
+"""Error handling"""
+"""AttributeError()
+ValueError() vs TypeError()
+ZeroDivisionError()
+NameError()
+IndexError()
+ImportError()"""
+
+"""Include a suite of test cases:
+make sure that all existing tests must continue to pass"""
+
+"""consider unpredictability of end useres:
+end-user keyboard input"""
+
