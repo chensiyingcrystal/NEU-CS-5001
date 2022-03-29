a51387fcce70b9528d03001e99d0ff48164044f8
Siying Chen <chensiyingcrystal@gmail.com>
2022-03-28 20:28:43 -0700

    Update Lecture8.py

--- Lecture8/Lecture8.py
+++ Lecture8/Lecture8.py
@@ -12,3 +12,7 @@ make sure that all existing tests must continue to pass"""
 """consider unpredictability of end useres:
 end-user keyboard input"""
 
+"""produce informative error messages"""
+
+
+
