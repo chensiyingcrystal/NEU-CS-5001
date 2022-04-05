8f10af3e923cfa90e45dae76a756a8496fa8e457
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-04 18:33:40 -0700

    update

---
+++ Lecture9/lecture9.py
@@ -0,0 +1,18 @@
+"""Aggregations of individual data elements
+strings; lists; tuplesl
+Other data structure: for expressive convenience; better performance
+Dictionary: unique key
+Set: math sets concept to organize data"""
+
+# Dictionary:
+# Key must be immutable, unique
+# Key value pair
+# {} delimit; 
+
+# Three different ways to initialize a dictionary:
+# dic = {'A':3, 'B':4} // most typical key-value pair
+# dic([('A',3), ('B', 4)])  // group key and value in a tuple format;convert tuples into dic
+# dic(A=3, B=4)  //similar to assigning variables to values; no need to quote
+
+# 'A' in dic // to check if the key is in the dictionary
+# This will return True
