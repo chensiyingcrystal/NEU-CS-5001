1b50fe643fbc6929e050a5e9dc3b00f703d78dc8
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-11 20:22:41 -0700

    Update lecture10.py

--- Lecture10/lecture10.py
+++ Lecture10/lecture10.py
@@ -6,3 +6,11 @@
 # return an object while there's no return keyword in the construct function
 # dot notaion to access attributesß and methods
 # a contructor must not explicitly return a value!
+
+"""__str__(self) method
+def __str__(self):
+    # tell python how to print the object
+    instead of class object + location
+    output = 
+    return output
+"""
