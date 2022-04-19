fb1ebe3fd5d53eba3fd568b26eadb29e3a342f13
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-19 10:06:00 -0700

    Update Lecture11.py

--- Lecture11/Lecture11.py
+++ Lecture11/Lecture11.py
@@ -3,4 +3,14 @@
 # useful for implementing your algorithms; 
 # functions call frames including binding to local variables
 # last in, first out(LIFO)
-# 
+# stack adt operations
+-- push(x)
+-- pop()
+-- is_empty()
+-- is_full()
+-- peek()
+-- count()
+
+# stack implementation
+-- from list:
+protect its function from changing of the list function
