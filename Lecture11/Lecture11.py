04d287ef2669b636e25a7b06a6a4628633865540
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-18 18:51:58 -0700

    update

---
+++ Lecture11/Lecture11.py
@@ -0,0 +1,6 @@
+"""Data structures and ADT(abstract data type)"""
+"""Stack ADT"""
+# useful for implementing your algorithms; 
+# functions call frames including binding to local variables
+# last in, first out(LIFO)
+# 
