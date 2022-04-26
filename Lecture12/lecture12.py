62ca3086f338a3eecb3a1ccc84923c3ee21ee837
Siying Chen <chensiyingcrystal@gmail.com>
2022-04-25 20:44:50 -0700

    update

---
+++ Lecture12/lecture12.py
@@ -0,0 +1,5 @@
+"""Algorithmic Efficiency"""
+# time and space complexity
+# interested in "tightest upper bound" of problem when the data volume grows
+# constant factors does not matter (for theoretical analysis)
+# how fast is it grow? 
